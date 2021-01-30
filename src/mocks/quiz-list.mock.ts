c008adf4c7423a732c90aa60af43097911d12e61
Bilal Lahmami <bilal.lahmami@etu.univ-cotedazur.fr>
2021-01-30 20:58:10 +0100

    Exercice 1

--- src/mocks/quiz-list.mock.ts
+++ src/mocks/quiz-list.mock.ts
@@ -1,29 +1,43 @@
 import { Quiz } from '../models/quiz.model';
 import { Question } from '../models/question.model';
 
-// export const QUESTION_ACTOR: Question = {
-//     label: 'Jean Gabin a joué dans...',
-//     answers: [
-//         {
-//             value: 'Les tuches II',
-//             isCorrect: false,
-//         },
-//         {
-//             value: 'La grande illusion',
-//             isCorrect: true,
-//         }
-//     ]
-// };
+export const QUESTION_ACTOR: Question = {
+    label: 'Jean Gabin a joué dans...',
+    answers: [
+        {
+            value: 'Les tuches II',
+            isCorrect: false,
+        },
+        {
+            value: 'La grande illusion',
+            isCorrect: true,
+        }
+    ]
+};
+
+export const QUESTION_SPORT: Question = {
+  label: 'Le vainqueur de la finale du 100 mètres aux Jeux Olympiques de 2016 est...',
+  answers: [
+    {
+      value: 'Usain Bolt',
+      isCorrect: true,
+    },
+    {
+      value: 'Sonic',
+      isCorrect: false,
+    }
+  ]
+};
 
 export const QUIZ_LIST: Quiz[] = [
     {
         name: 'Les Acteurs', // What's happening if I change this value..?
         theme: 'Actor',
-        questions: [],
+        questions: [QUESTION_ACTOR],
     },
     {
-        name: 'Les Films',
-        theme: 'Movie',
-        questions: [],
+        name: 'Les Sports',
+        theme: 'Sport',
+        questions: [QUESTION_SPORT],
     }
 ];
